a3852964fc6e5657b93c7cc6afbde504b4f77c0f
Victor Valdivia <kazter99@gmail.com>
2021-11-01 23:07:57 -0300

    imagen, mas boton que muestra cosas por consola, y todo centrado

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+import {Text, View, StyleSheet, Image, Button} from 'react-native';
+const App = () =>{
+  return <View style={styles.container}>
+      <Text style={styles.title}>Hello World</Text>
+      <Image style={styles.imgsize} source={{uri:'https://i.pinimg.com/736x/e1/be/d7/e1bed7ca54e89b5af9afb815b7f1bdba.jpg'}}/>
+      <Button 
+        title='Dale Click'
+        color='#DC143C'
+        onPress={ () => console.log('Hello World')}
+      />
     </View>
-  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'center', 
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: "#292929"},
+  title: {
+    fontSize: 30,
+    color: "#ffffff",
+    marginBottom: 18},
+  imgsize: {
+    height: 200,
+    width:200,
+    borderRadius: 100,
+    marginBottom: 18}
+})
+
+
+export default App;
+
